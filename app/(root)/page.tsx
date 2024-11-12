1b3e9fd8b86b46f572ae131474bdc3a6128d311b
Sumant Patel <patelsumant19@gmail.com>
2024-11-12 16:59:42 +0530

    Added: the search bar with server action

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,24 @@
-export default function Home() {
-  console.log("What am I doing here? == SERVER or Client");
+import SearchForm from "@/components/SearchForm";
 
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Statup, <br />
+          Connect with Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competition.
+        </p>
+
+        <SearchForm query={query} />
+      </section>
     </>
   );
 }
