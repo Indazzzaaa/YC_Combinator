552b58937eb780723e594263421678a3e3e2c553
Sumant Patel <patelsumant19@gmail.com>
2024-11-13 16:15:28 +0530

    Added: the sanity live fetch, so that we don't have to disable the CDN and data can be fetch as new ones added just like ISR

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
-import { client } from "@/sanity/lib/client";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({
@@ -10,7 +10,7 @@ export default async function Home({
 }) {
   const query = (await searchParams).query;
 
-  const posts = await client.fetch(STARTUPS_QUERY);
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
   console.log(JSON.stringify(posts, null, 2));
 
   return (
@@ -41,6 +41,8 @@ export default async function Home({
           )}
         </ul>
       </section>
+      {/*  this will fetch the live data as soon as new content added so , cdn feture is enabled and live data is also fetched.(kind of ISR) */}
+      <SanityLive />
     </>
   );
 }
